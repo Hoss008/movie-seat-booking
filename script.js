014bef6aa4b9ae48a1e1bb6fd02fc9fc442da80e
Hoss008 <43291152+Hoss008@users.noreply.github.com>
2025-08-30 18:12:56 +0300

    managed to select/unselect the seat with changing the total price

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 var chosenSeat = document.querySelectorAll(".seat:not(.occupied)");
-var count = document.querySelector("#count");
+var countval = parseInt(document.querySelector("#count").textContent);
+var count = document.getElementById("count");
+var total = document.querySelector("#total");
+var movie = document.getElementById("movie");
+var price = 10;
+let totalval = 0;
 
 chosenSeat.forEach(function (seat) {
   seat.addEventListener("click", function () {
     if (seat.classList.toggle("selected")) {
-      count.textContent = parseInt(count.textContent) + 1;
+      countval++;
+
+      totalval = totalval + price;
+      count.textContent = `${countval}`;
     } else {
-      count.textContent = parseInt(count.textContent) - 1;
+      countval--;
+      totalval = totalval - price;
+      count.textContent = `${countval}`;
     }
+    total.textContent = parseInt(totalval);
   });
+});
+
+movie.addEventListener("change", function (e) {
+  price = +e.target.value;
 });
